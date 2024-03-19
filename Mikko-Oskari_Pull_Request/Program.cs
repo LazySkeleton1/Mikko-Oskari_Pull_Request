4cf9b834ce67ffe0963669313860a47aa5be8b7b
Mikko Sivonen <mikkosivonen6@gmail.com>
2024-03-19 09:59:22 +0200

    Add project files.

---
+++ Mikko-Oskari_Pull_Request/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mikko_Oskari_Pull_Request
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
